666e5934e72bb680b86edb3b962f4a27933c124c
chaoowang <chaohsuaan@gmail.com>
2022-03-20 05:33:03 +0800

    Setup button handler

--- app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
@@ -3,6 +3,9 @@ package au.edu.jcu.cp3406.quicksum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int sum;
+
+    public void buttonClicked(View view) {
+        Button button = (Button) view;
+
+        int number = Integer.parseInt(button.getText().toString());
+        sum += number;
+
+        TextView textView = (TextView) findViewById(R.id.sum);
+        String result = "" + sum;
+        textView.setText(result);
+    }
+
 }
