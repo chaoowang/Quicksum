4d465539270fb7a539a82ae5c4ee806576be4787
chaoowang <chaohsuaan@gmail.com>
2022-03-24 04:21:42 +0800

    apply OnClickListener

--- app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/quicksum/MainActivity.java
@@ -13,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button1);
+        btn1.setOnClickListener(v -> buttonClicked(btn1));
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(v -> buttonClicked(btn2));
+        Button btn3 = findViewById(R.id.button3);
+        btn3.setOnClickListener(v -> buttonClicked(btn3));
+        Button btn4 = findViewById(R.id.button4);
+        btn4.setOnClickListener(v -> buttonClicked(btn4));
+        Button btn5 = findViewById(R.id.button5);
+        btn5.setOnClickListener(v -> buttonClicked(btn5));
+        Button btn6 = findViewById(R.id.button6);
+        btn6.setOnClickListener(v -> buttonClicked(btn6));
+        Button btn7 = findViewById(R.id.button7);
+        btn7.setOnClickListener(v -> buttonClicked(btn7));
+        Button btn8 = findViewById(R.id.button8);
+        btn8.setOnClickListener(v -> buttonClicked(btn8));
+        Button btn9 = findViewById(R.id.button9);
+        btn9.setOnClickListener(v -> buttonClicked(btn9));
+        Button clearBtn = findViewById(R.id.clearbtn);
+        clearBtn.setOnClickListener(v -> clearSum(clearBtn));
     }
 
     int sum;
